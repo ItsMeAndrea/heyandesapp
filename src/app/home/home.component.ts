56438f52e45f241142bfa7ea4f17a8c5db1ea85f
ItsMeAndrea <itsandrea_94@hotmail.com>
2021-01-07 22:19:33 -0400

    moth with more sales

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -52,4 +52,63 @@ export class HomeComponent implements OnInit {
       this.rows.push(rowData);
     });
   }
+
+  getMoreSales() {
+    const totalSales = this.rows.map((empresa) => empresa.totalSales);
+
+    return Math.max(...totalSales);
+  }
+
+  monthWithMoreSales() {
+    const meses = this.empresas.map((empresa) => {
+      const date = new Date(empresa.datePayment);
+      const month = date.getMonth();
+      return month;
+    });
+    const uniqueSet = new Set(meses);
+    const uniqueMeses = [...uniqueSet];
+
+    let monthData = [];
+
+    const monthName = [
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre',
+    ];
+
+    uniqueMeses.forEach((mes) => {
+      const datosPorMes = this.empresas.filter((data) => {
+        const dataMeses = new Date(data.datePayment).getMonth();
+
+        return dataMeses === mes;
+      });
+
+      const totalMes = datosPorMes
+        .map((data) => data.finalPrice)
+        .reduce((a, b) => a + b, 0);
+
+      const data = {
+        mes,
+        totalMes,
+      };
+
+      monthData.push(data);
+    });
+
+    const mothWithMoreSales = monthData.reduce(
+      (a, b) => (a = a > b.totalMes ? a : b.mes),
+      0
+    );
+
+    return monthName[mothWithMoreSales];
+  }
 }
